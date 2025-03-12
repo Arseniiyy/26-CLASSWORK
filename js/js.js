766eaee782a7374a3d861a61f6bb5ddc5fea7ef0
Арсений <Penkovskiy2024@mail.ru>
2025-03-12 21:26:19 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,89 @@
+// let year = prompt("какой год?")
+// if (year < 2015) {
+//     alert("старик")
+// } else if (year > 2015) {
+//     alert("бро,это будущее")
+// } else {
+//     alert("хорош")
+// } else {
+//     ("Error")
+// }
+// let value = Number(prompt('Введите число'))
+// if(value == 1){
+//     alert("верно")
+// }else{
+//     alert('err')
+// }
+
+
+// let accsessAllowed;
+// let age = prompt('Сколько вам лет')
+// if( age > 18){
+//     accsessAllowed = true
+// }else {
+//     accsessAllowed = false
+// }
+// console.log(accsessAllowed)
+
+
+// let name = prompt("какое офицальное название java script")
+// if(name = "ECMAScript"{
+//     alert("Верно")
+// }else{
+//     alert("Не Знаете?ECMAScript")
+// }
+
+
+// let cifra = prompt("Введи число")
+// if (cifra > 1) {
+//     alert(1)
+// } else if (cifra < 1){
+//     alert(-1)
+// } else if (cifra == 0){
+//     alert(0)
+// }
+
+
+// let result;
+// if (a+b<4){
+//     result ='Мало';
+// }else{
+//     result = 'Много'
+// }
+
+
+
+// let result = (5+3<18) ? true : false
+// alert(result)
+// // Тернарный оператор
+
+
+
+// let message = (login == "Сотрудник") ? "Привет": (login == "Директор") ? "Здравствуйте": (login == '') ? "Нет логина": ''
+
+
+// let i = 0;
+// while (i < 10){
+//     console.log(i)
+//     i--;
+// }
+
+
+
+
+// let sum = 0
+
+// while(true)[
+//     lot value = +prompt("Введите число",'')
+//     if(!value)break;
+//     sum +-!value
+// ]
+// alert('Сумма' + sim)
+
+
+
+
+// for(let i = 0; i < 10;i++){
+//     if(i % 2 ==0)continue;
+//     alert(i)
+// }
