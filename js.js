4ebc9103e6eee236b9ca6a17a9e8807e7487e972
Арсений <Penkovskiy2024@mail.ru>
2025-03-17 21:10:59 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,86 @@
+// let number = prompt("Введите число")
+// number = parseInt(number)
+// for {let i =1; i<=number;i++){
+// if (number % i ===0){
+//     console.log(i)
+// }
+// }
+
+// let number = prompt('Введите число')
+// number = perseInt(number)
+
+// let factorial = 1;
+// let i = 1;
+
+// while(i <= number){
+//     factorial *-i
+//     i++
+// }
+// console.log("Факторные числа" + number + "равен" + factorial)
+
+
+
+// let a = 2 +2 
+// switch(a){
+//     case 3:
+//     alert("Маловато")
+//     break
+//     case 4:
+//     alert("Вы правы")
+//     break
+//     case 5:
+//     alert("Много")
+//     break
+//     default:
+//         alert("Значение отсутствует")
+
+// }
+
+
+
+// switch (a) {
+//     case 4:
+//         alert("Правильно");
+//         break;
+//     case 3:
+//     case 5:
+//         alert("Не правильно")
+//         break;
+//         default:
+//             alert("Странно")
+// }
+
+// Задача 1
+// let i = prompt("Введите ваш брузер")
+// if(`Chrome,FireFox,Safari,Opera`){
+//     alert(`Okay we support these browers too`)
+// }
+// else if(`others`){
+//     alert(`Okay we not support these browers too`)
+// }
+
+
+
+// function printword(value, value1){
+//     alert(value, value1)
+// }
+// printword("YOOOO")
+
+
+// function add(a,b){
+//     return a + b
+// }
+// alert(add(2,200))
+
+
+//Задание 1
+
+function add(a,b){
+    return a + b
+}
+alert(add(2,5))
+
+//Задание 2
+Math.max(10, 20){
+    return
+}
