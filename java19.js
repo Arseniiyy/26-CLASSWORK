75ccf6a6d7f9074f4315a92b682fb5a072836de2
Арсений <Penkovskiy2024@mail.ru>
2025-03-19 21:10:10 +0300

    Add files via upload

---
+++ java19.js
@@ -0,0 +1,170 @@
+// let value1 = +prompt("Введите первое значение")
+// let value2 = +prompt("Введите второе значение")
+// let value3 = prompt("Введите третье значение")
+// let result;
+// switch(value3){
+//     case`+`:
+//     result = value1 + value2;
+//     break;
+//     case`-`:
+//     result = value1 + value2;
+//     break;
+// }
+// alert(result)
+
+
+
+
+// function facrorial()
+//     let i = 1
+//     let number = 5
+//     while(i <= number){
+//         i *=number
+//         i++
+//     }
+//     alert(facrorial())
+
+
+// let number = prompt("Введите число"){
+//     value a = 5;
+// value b = 3;
+// double sum = a + b;
+// double product = a * b
+// }
+// alert(value)
+// не правильно
+
+
+
+
+
+
+// let world = function({
+//     alert("Привет")
+// })
+
+
+// let world = function({
+//     alert("Академия топ")
+// })
+// int  func = world
+
+
+//function showOk,showCancel
+// function ask(question,yes,no) {
+//     if (confirm(question))yes()
+//     else no()
+// }
+// function showOk(){
+//     alert("вы согласны")
+// }
+// function showCancel(){
+//     alert("Отмена")
+// }
+// ask("вы согласны?",showOk,showCancel)
+
+
+//привет/здравствуйте
+// let age = prompt("Сколько вам лет")
+// if(age < 18){
+//     welcome = function(){
+//         alert("привет")
+//     };
+
+// }else{
+//     welcome = function(){
+//         alert('Здравствуйте')
+//     }
+// }
+// welcome(18)
+
+
+
+
+// // //задание 1
+// function ask(question,yes,no) {
+//     if (confirm(question))yes()
+//     else no()
+// }
+// function showOk(){
+//     alert("Привет")
+// }
+// function showCancel(){
+//     alert("Здравствуйте")
+// }
+// ask("Say Hello",showOk,showCancel)
+
+
+//Задание 2
+// function ask(question,yes,no) {
+//     if (confirm(question))yes()
+//     else no()
+// }
+// function showOk(){
+//     alert("this is callback")
+// }
+// function showCancel(){
+//     alert("i don`t know")
+// }
+// ask("What is callback",showOk,showCancel)
+
+
+//Задание 3  multiply
+// let multiply = function(a,b){
+//     return a * b;
+// }
+// alert(multiply(4,9))
+
+
+//Задание 4
+// let calculator = function(a,b,callback){
+// return callback(a,b)
+// }
+// let add = function(x,y){
+//     return x * y
+// }
+// alert(calculator(4,8,add))
+
+
+//Задание 5
+// let ask = function(callback){
+//     callback("Как твои делаа")
+// }
+// let show = function(question){
+//     alert(question)
+// }
+// ask(show)
+
+
+
+
+// let sum = a => a + 10
+// alert (sum(1,2))
+
+
+
+// let age = prompt("Твой возраст")
+// let welcome = (age < 19) ?
+// () =>alert("Привет")
+// () =>alert("Салам")
+
+
+// //Сумма
+// let sum =(a,b) =>{
+//     let result = a + b;
+//     return result
+// }
+// alert(sum(4,8))
+
+
+//Задача
+// function ask(question,yes,no){
+//     if(confirm(question))yes()
+//     else no();
+// }
+// ask(
+//     "Согласен?"
+//     () => alert("Вы согласились"),
+//     () => alert("Вы отменили выполнение"),
+// )
+
