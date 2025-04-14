c5c73bba4feb84dbac609c230d4283a96ec6e422
Арсений <Penkovskiy2024@mail.ru>
2025-04-14 21:13:07 +0300

    Add files via upload

---
+++ 14.04/main.js
@@ -0,0 +1,75 @@
+// // let array = ['Regina','Egorka','Elia']
+// // //параметры принимают значения от и до
+// // array.splice(0,1)
+// // console.log(array)
+
+
+// // array.splice(1,2,"Robert","Egor2")
+// // console.log(array)
+
+
+// let array = ['Regina',"Egorka","Marta"]
+// console.log(array.slice([]))
+
+
+// let array = ['Regina',"Egorka","Marta"]
+// console.log(array.slice())
+
+
+// let array = [1,2]
+// console.log(array.concat([3,4]))
+
+// let array = [1,2]
+// let arr =[3,4]
+// console.log(array.concat(arr[0]))
+// //добавляет arr в существующий  array
+
+// let array = ['Regina',"Egorka","Marta"]
+// array.forEach(alert)
+
+
+// let array = ['Regina',"Egorka","Marta"].forEach((item,index,array)=>){
+//     console.log('У ${item} индекс ${index}, в массиве ${array}')
+// }
+
+
+// let array = ['Regina',"Egorka","Marta","Elya"];
+// alert(array.indexOf("Regina"))
+
+
+// let array = ['Regina',"Egorka","Marta","Elya"];
+// console.log(array.indexOf(NaN))
+// console.log(array.includes(4))
+
+
+// let admin = [
+//     {id:1, name:'Regina'},
+//     {id:2, name:'Marta'},
+//     {id:2, name:'Egor'}
+// ]:
+// let find = admin.find(item => item.id == 2);
+// alert(find.name)
+
+
+
+// let array = ['Regina',"Egorka","Marta","Elya","Robert","VAZgen"];
+// let length = array.map(item => item.length);
+// console.log(length)
+// let target = "regina"
+// let length = array.map((item,index))=>
+//     index ==target?item.length:alert("error")
+//     console.log(length)
+
+
+//  let array = ['Regina',"Egorka","Marta","Elya","Robert","VAZgen"];
+//  let target = "regina"
+
+//  let lowerCase = array.map(item => item.toLowerCase());
+//  let targetIndex = lowerCase.indexOf(target)
+
+//  let len = lowerCase.map((item,index))=>
+//  index === targetIndex?item.length : undefined
+
+//  console.log(len)
+
+
